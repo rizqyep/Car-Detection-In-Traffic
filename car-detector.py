959cd5f58226ab2d331730fd64abb25a65a8e788
rizqyep <rizqyepr@gmail.com>
2019-11-22 01:49:03 +0700

    Python Code to Detect Car in Traffic

---
+++ car-detector.py
@@ -0,0 +1,22 @@
+import cv2
+
+detect='cars.xml'
+vid='video2.avi'
+cap=cv2.VideoCapture(vid)
+car_detect=cv2.CascadeClassifier(detect)
+
+while True :
+    ret,img=cap.read()
+    if(type(img)==type(None)):
+        break
+    grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cars = car_detect.detectMultiScale(gscale,scaleFactor=1.1,minNeighbors=1)
+
+    for(x,y,w,h) in cars:
+        cv2.rectangle(gbr,(x,y),(x+w,y+h),(0,255,0),2)
+
+    cv2.imshow('video',img)
+    if cv2.waitKey(33)==27:
+        break
+
+cv2.destroyAllWindows()
