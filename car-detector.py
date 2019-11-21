a9edc32753be854f047bb78fdf284371db1b72ab
rizqyep <rizqyepr@gmail.com>
2019-11-22 02:13:37 +0700

    Update car-detector.py

--- car-detector.py
+++ car-detector.py
@@ -1,16 +1,16 @@
 import cv2
 
 detect='cars.xml'
-vid='video2.avi'
+vid='video2.avi' #change this into your video path file
 cap=cv2.VideoCapture(vid)
 car_detect=cv2.CascadeClassifier(detect)
 
 while True :
-    ret,img=cap.read()
-    if(type(img)==type(None)):
+    ret,img=cap.read() 
+    if(type(img)==type(None)): # 
         break
-    grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cars = car_detect.detectMultiScale(gscale,scaleFactor=1.1,minNeighbors=1)
+    grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #convert the frame into grayscale format
+    cars = car_detect.detectMultiScale(gscale,scaleFactor=1.1,minNeighbors=1) #adjust value int the scaleFactor and minNeighbors to improve accuracy
 
     for(x,y,w,h) in cars:
         cv2.rectangle(gbr,(x,y),(x+w,y+h),(0,255,0),2)
